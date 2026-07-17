1b44473beae2f234a35a67eca2780c4f7115aca4
Manmohit Singh <manmohit249@gmail.com>
2026-07-17 23:14:33 +0530

    feat(server): ad prisma connection and shutdown

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -3,17 +3,23 @@ import { env } from "@/config/env.js";
 
 import { logger } from "@/shared/logger.js";
 import { app } from "@/app.js";
+import { prisma } from "@/shared/prisma.js";
 
-const server = app.listen(env.PORT, () => {
-  logger.info(
-    {
-      port: env.PORT,
-      env: env.NODE_ENV,
-    },
-    "Server started"
-  );
-});
+let server: ReturnType<typeof app.listen>;
 
+await prisma.$connect();
+try {
+  await prisma.$connect();
+
+  server = app.listen(env.PORT, () => {
+    logger.info({ port: env.PORT, env: env.NODE_ENV }, "Server started");
+  });
+} catch (error) {
+  logger.fatal(error, "Failed to start server.");
+  process.exit(1);
+}
+
+// Shutting Down Application
 process.on("SIGINT", () => shutdown("SIGINT"));
 process.on("SIGTERM", () => shutdown("SIGTERM"));
 
@@ -23,15 +29,22 @@ async function shutdown(signal: "SIGINT" | "SIGTERM") {
   if (isShuttingDown) return;
   isShuttingDown = true;
 
-  logger.info(`${signal} received. Shutting down...`);
+  logger.info({ signal }, "Shutdown initiated");
 
-  server.close((error) => {
+  server.close(async (error) => {
     if (error) {
       logger.error(error, "Failed to close HTTP server.");
       process.exit(1);
     }
     logger.info("HTTP server closed.");
-    // Add shutdowns of app's processes here
+
+    try {
+      await prisma.$disconnect();
+      logger.info("Database connection closed.");
+    } catch (error) {
+      logger.error(error, "Failed to close database connection.");
+    }
+
     process.exit(0);
   });
 }
