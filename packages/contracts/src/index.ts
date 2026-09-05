e831d6a160449f00cb4383602bec2e453aaa5d4c
Manmohit Singh <manmohit249@gmail.com>
2026-09-06 03:08:06 +0530

    refactor(contracts): refactor '@repo/contracts'

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -1,6 +1,6 @@
 export { ErrorCode } from "./errorCodes.js"; // type and object
 export type { ApiResponse, ErrorResponse, SuccessResponse, ErrorFields } from "./response.js";
 
-export * from "./validation/index.js";
-
 export * from "./auth/index.js";
+
+export * from "./user/index.js";
